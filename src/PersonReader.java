6c3def253a6f4647f4f9a41afabdba2e0ad9f9dd
Scott Browning <scottybrowning12345@gmail.com>
2025-01-27 16:21:34 -0500

    Refractoring the PersonReader.java added a array list, removed some comments. Added a new catch as well

--- src/PersonReader.java
+++ src/PersonReader.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -14,28 +15,45 @@ import java.util.Scanner;
             JFileChooser chooser = new JFileChooser();
             Scanner inFile;
             String line;
+            //added ArrayList to store people
+            ArrayList<Person> people = new ArrayList<>();
+
+
             Path target = new File(System.getProperty("user.dir")).toPath();
             target = target.resolve("src");
             // set the chooser to the project src directory
             chooser.setCurrentDirectory(target.toFile());
 
-            try  // Code that might trigger the exception goes here
+            try
             {
 
                 if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                     target = chooser.getSelectedFile().toPath();  // this is a File object not a String filename
-                    // Print the header
-                    System.out.println(String.format("%-6s %-8s %-5s %-5s %-5s", "ID", "FirstName", "LastName", "Title", "YOB"));
-                    System.out.println("-------------------------------------------------");
 
                     inFile = new Scanner(target);
 
                     while (inFile.hasNextLine()) {
                         line = inFile.nextLine();
                         System.out.println(line);
+                        String[] fields = line.split(",");
+                    }
+                    String ID = fields[0].trim();
+                    String firstName = fields[1].trim();
+                    String lastName = fields[2].trim();
+                    String title = fields[3].trim();
+                    int YOB = Integer.parseInt(fields[4].trim());
+
+                    Person person = new Person(firstName, lastName, ID, title, YOB);
+                    people.add(person);  // Add the Person to the list
+                //moved print header here
+                    // Print the header
+                    System.out.println(String.format("%-6s %-8s %-5s %-5s %-5s", "ID", "FirstName", "LastName", "Title", "YOB"));
+                    System.out.println("-------------------------------------------------");
+                    for (Person Person : people) {
+                        System.out.println(String.format("%-10s %-15s %-15s %-10s %-5d",
+                                Person.getID(), Person.getFirstName(), Person.getLastName(), Person.getTitle(), Person.getYOB()));
                     }
 
-                    inFile.close();
                 } else   // User did not pick a file, closed the chooser
                 {
                     System.out.println("Sorry, you must select a file! Termininating!");
@@ -47,6 +65,10 @@ import java.util.Scanner;
             } catch (IOException e) // code to handle this exception
             {
                 System.out.println("IOException Error");
+                e.printStackTrace();
+            }
+            catch (Exception e) {
+                System.out.println("An error occurred while processing the file.");
                 e.printStackTrace();
             }
         }
